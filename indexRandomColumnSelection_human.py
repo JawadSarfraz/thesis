d2bcfcfaa3dbc97c0ee56bce1329e68ad06f7257
Jawwad Bin Sarfraz <j.sarfraz@appfarms.com>
2023-12-06 12:52:20 +0100

    Human data for 2Dimestions

---
+++ indexRandomColumnSelection_human.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+# Path to the Excel file
+file_path = 'data.xlsx'
+
+df = pd.read_excel(file_path,7)
+
+# print(df.iloc[:, 10].unique())
+
+# # Check the data type of column J
+# print(df.iloc[:, 10].dtype)
+df.replace('', pd.NA, inplace=True)
+
+# Exclude columns
+# combination with 8th columns...
+columns_to_exclude = [9,10,11,12]
+df_modified = df.drop(df.columns[columns_to_exclude], axis=1)
+
+#df_modified.iloc[:, 9] = df_modified.iloc[:, 9].round(2)
+
+# Remove rows with any missing values in the remaining columns
+df_cleaned = df_modified.iloc[:, 0:13].dropna()
+
+# Save the cleaned data to a new Excel file
+cleaned_file_path = 'resultFile_1.xlsx'
+df_cleaned.to_excel(cleaned_file_path, index=False)
